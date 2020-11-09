b6795da9cad7df4ce242c33dd40f5c87ddad4035
Paul Williams <paul.d.williamsxiv@gmail.com>
2020-11-08 21:33:21 -0500

    [project] changed README as well as added movement area to find battles

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const body = document.querySelector('body')
 const startScreen = document.querySelector('#start-screen')
 const battleScreen = document.querySelector('#battle-screen')
 const mainGame = document.querySelector('#main-game')
-const ctx = mainGame.getContext('2d')
 const rules = document.querySelector('#rules')
 //menu button
 const menuBtn = document.querySelector('#menu-btn')
 //playmap
 const playarea = document.querySelector('#playarea')
+const character = document.querySelector(".character");
 //healthbars
 const enemyHealthBar = document.querySelector('#enemy-bar')
 const playerHealthBar = document.querySelector('#player-bar')
@@ -20,6 +20,7 @@ const attackMoves = [25,bugAttack,10,40]
 //battle options
 const rageQuit = document.querySelector('#rage-quit')
 const fixKit = document.querySelector('#fix-kit')
+
 
 //
 const computedStyle = getComputedStyle(mainGame)
@@ -115,22 +116,34 @@ const beginGame =(Player) =>{
 const onCanvas = () =>{
     
 }
+const resetHealth =(Player,enemyHealth)=>{
+    setTimeout(()=>{
+        enemyHealth = 100
+        Player.health = 100
+        enemyHealthBar.style.width = `${enemyHealth}%`
+        playerHealthBar.style.width = `${Player.health}%`
+        return enemyHealth, Player
+    },5000)
+}
+
 //check the health of players
 const checkHealth = (enemyHealth,Player) =>{
-    console.log(enemyHealth);
-    console.log(Player.health);
-    if (enemyHealth <= 0 || Player.health <= 0){
-        if (Player.Health <=0){
-            console.log('enemy wins');
-        } else if (enemyHealth <= 0){
 
-            console.log(`${Player.name} wins`);
+    if (enemyHealth <= 0 || Player.health <= 0){
+        if (Player.health <=0){
+            resetHealth(Player,enemyHealth)
+
+        } else if (enemyHealth <= 0){
+            resetHealth(Player,enemyHealth)
+
         }
         setTimeout(()=>{
             startScreen.style.zIndex = 0
             rules.style.zIndex = 0
             mainGame.style.zIndex = 1
             battleScreen.style.zIndex = 0
+            enemyHealthBar.style.width = `${enemyHealth}%`
+            playerHealthBar.style.width = '100%'
         },2000)
     }
 }
@@ -144,12 +157,14 @@ const restoreHealth = (Player) =>{
     })
 }
 //rage quit
-const leaveBattle = () =>{
+const leaveBattle = (Player,enemyHealth) =>{
     rageQuit.addEventListener('click',()=>{
         startScreen.style.zIndex = 0
         rules.style.zIndex = 0
         mainGame.style.zIndex = 1
         battleScreen.style.zIndex = 0
+
+        resetHealth(Player,enemyHealth)
     })
 }
 //enemy attacks
@@ -160,34 +175,41 @@ const enemyAttack = (Player) =>{
         playerHealthBar.style.width = `${Player.health}%`
     },2000)
 }
-const attackButtons = (enemyHealth,Player) =>{
+const attackButtons = (Player,enemyHealth) =>{
     for (let i = 0; i < attacks.length; i++){
         attacks[i].addEventListener('click',()=>{
             enemyHealth = enemyHealth - attackMoves[i]
             if (Player.health <= 0){
                 Player.health = 0
+                setTimeout(()=>{
+                    Player.health = 100
+                    enemyHealth = 100
+                },3000)
             }
             if (enemyHealth <= 0){
                 enemyHealth = 0
+                setTimeout(()=>{
+                    Player.health = 100
+                    enemyHealth = 100
+                },3000)
             }
             enemyHealthBar.style.width = `${enemyHealth}%`
             checkHealth(enemyHealth,Player)
             enemyAttack(Player)
+            
         })
+        
     }
 }
-const battleLogic = (Player) =>{
-    let enemyHealth = 0
-    enemyHealth += 100
+const battleLogic = (Player,enemyHealth) =>{
     enemyHealthBar.style.width = `${enemyHealth}%`
     playerHealthBar.style.width = `${Player.health}%`
-    leaveBattle()
-    attackButtons(enemyHealth, Player)
+
+    leaveBattle(Player,enemyHealth)
+    attackButtons(Player,enemyHealth)
     restoreHealth(Player)
-    setTimeout(()=>{
-        enemyHealth += 25
-        enemyHealthBar.style.width = `${enemyHealth}%`
-    },20000)
+    
+    
 }
 
     
@@ -226,11 +248,88 @@ const menuButton = () =>{
         console.log(`rules z index${rules.style.zIndex}\nstart screen z index${startScreen.style.zIndex}\nmainGame z index${mainGame.style.zIndex}\nbattle screen z index ${battleScreen.style.zIndex}`);
     })
 }
+
+var x = 90;
+var y = 34;
+var held_directions = []; //State of which arrow keys we are holding down
+var speed = 1; //How fast the character moves in pixels per frame
+
+const placeCharacter = () => {
+    
+    var pixelSize = parseInt(
+        getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
+    );
+    
+    const held_direction = held_directions[0];
+    if (held_direction) {
+        if (held_direction === directions.right) {x += speed;}
+        if (held_direction === directions.left) {x -= speed;}
+        if (held_direction === directions.down) {y += speed;}
+        if (held_direction === directions.up) {y -= speed;}
+        character.setAttribute("facing", held_direction);
+    }
+    character.setAttribute("walking", held_direction ? "true" : "false");
+    
+    //Limits (gives the illusion of walls)
+    var leftLimit = -8;
+    var rightLimit = (16 * 11)+8;
+    var topLimit = -8 + 32;
+    var bottomLimit = (16 * 7);
+    if (x < leftLimit) { x = leftLimit; }
+    if (x > rightLimit) { x = rightLimit; }
+    if (y < topLimit) { y = topLimit; }
+    if (y > bottomLimit) { y = bottomLimit; }
+    
+    
+    var camera_left = pixelSize * 66;
+    var camera_top = pixelSize * 42;
+    
+    playarea.style.transform = `translate3d( ${-x*pixelSize+camera_left}px, ${-y*pixelSize+camera_top}px, 0 )`;
+    character.style.transform = `translate3d( ${x*pixelSize}px, ${y*pixelSize}px, 0 )`;  
+}
+const step = () => {
+    placeCharacter();
+    window.requestAnimationFrame(() => {
+    step();
+    })
+}
+step(); 
+
+
+    /* Direction key state */
+    const directions = {
+        up: "up",
+        down: "down",
+        left: "left",
+        right: "right",
+    }
+    const keys = {
+        38: directions.up,
+        37: directions.left,
+        39: directions.right,
+        40: directions.down,
+    }
+    document.addEventListener("keydown", (e) => {
+        var dir = keys[e.which];
+        if (dir && held_directions.indexOf(dir) === -1) {
+        held_directions.unshift(dir)
+        }
+    })
+    
+    document.addEventListener("keyup", (e) => {
+        var dir = keys[e.which];
+        var index = held_directions.indexOf(dir);
+        if (index > -1) {
+        held_directions.splice(index, 1)
+    }
+});
+
 const game = () =>{
+    let enemyHealth = 100
     const mainCharacter = new Player
     beginGame(mainCharacter)
     
-    battleLogic(mainCharacter)
+    battleLogic(mainCharacter,enemyHealth)
     // mapMovement(playarea)
         
         
